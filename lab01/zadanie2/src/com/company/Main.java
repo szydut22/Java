f4821c19f136e47f57e8d4ac5d1ba6b3f52d4d2d
AmazingWebistes <szymondutka3@gmail.com>
2021-10-05 17:24:23 +0200

    working exercise

--- lab01/zadanie2/src/com/company/Main.java
+++ lab01/zadanie2/src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
+import java.util.Scanner;
+import java.math.RoundingMode;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner s = new Scanner(System.in);
+	    double sum = 0, harmonic;
+
+	    System.out.println("Z ilu liczb chcesz policzyc srednia harmoniczna: ");
+	    int n = s.nextInt();
+
+        double[] harmonicTab = new double[n];
+
+        System.out.println("Z jakich liczb chcesz policzyc srednia harmoniczna: ");
+
+        for(int i = 0; i < n; i++) {
+            double num = s.nextDouble();
+            harmonicTab[i] = 1/num;
+            sum += harmonicTab[i];
+        }
+
+        harmonic = n/sum;
+
+        System.out.println("oto twoja srednia harmoniczna: " +harmonic);
     }
 }
