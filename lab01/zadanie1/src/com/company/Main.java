0221de6c2c224659beff4b41763ec0fd2f546a66
AmazingWebistes <szymondutka3@gmail.com>
2021-10-05 16:58:48 +0200

    working exercise

--- lab01/zadanie1/src/com/company/Main.java
+++ lab01/zadanie1/src/com/company/Main.java
@@ -9,19 +9,22 @@ public class Main {
         System.out.println("Podaj ilosc liczb ktore chcesz wprowadzic");
         int size = s.nextInt();
         int[] tab = new int[size];
-        int min = tab[0];
-        int max = tab[0];
 
         System.out.println("Wprowadz swoje liczby: ");
         for(int i = 0; i < size; i++) {
             int n = s.nextInt();
             tab[i] = n;
         }
+
+        int min = tab[0];
+        int max = tab[0];
+
         for(int i = 0; i < size; i++) {
             if(tab[i] < min){
                 min = tab[i];
             }
         }
+
         for(int i = 0; i < size; i++) {
             if(tab[i] > max){
                 max = tab[i];
