d354f3599cc77b6531a32929c049e355e2226412
AmazingWebistes <szymondutka3@gmail.com>
2021-10-05 18:31:56 +0200

    working exerciese

---
+++ lab01/zadanie5/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+	    Scanner s = new Scanner(System.in);
+
+        boolean automorficzna;
+	    System.out.println("Sprawdz czy twoja liczba jest liczba automorficzna: ");
+	    int num = s.nextInt();
+
+	    automorficzna = czyAutomorficzna(num);
+
+	    if(automorficzna == true){
+            System.out.println("Twoja liczba jest automorficzna");
+        }else{
+            System.out.println("Twoja liczba nie jest automorficzna");
+        }
+    }
+
+    static int wielkosc(int num){
+        int counter = 0;
+        while(num > 0){
+            counter++;
+            num /= 10;
+        }
+        return counter;
+    }
+
+    static boolean czyAutomorficzna(int num){
+        int automorficzna = num * num;
+        int size = wielkosc(num);
+
+        if(automorficzna%Math.pow(10,size) == num){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
